ba7d38cc854fdebd5fc68db08953fea67f1e8cef
Agnitica <37901353+Agnitica@users.noreply.github.com>
2018-07-31 08:13:38 +0530

    Add files via upload

---
+++ Part-1/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	// Use this for initialization
+	public PlayerController player; 
+	private float z;
+
+	void Start () {
+		z= transform.position.z - player.transform.position.z; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3 (transform.position.x , transform.position.y, player.transform.position.z+z);
+	}
+}
