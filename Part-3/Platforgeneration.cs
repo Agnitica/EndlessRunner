c8cb614df273f37ed8c40e3861f4306f6d4e4f15
Agnitica <37901353+Agnitica@users.noreply.github.com>
2018-08-07 15:44:03 +0530

    Add files via upload

---
+++ Part-3/Platforgeneration.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platforgeneration : MonoBehaviour {
+
+	public GameObject[] p;
+	public GameObject Player;
+	private Vector3 d;
+	public Pooler pool;
+
+
+
+	// Use this for initialization
+	void Start () {
+
+		d =   transform.position-Player.transform.position;
+
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.z - Player.transform.position.z < d.z)
+		{
+			//GameObject pt = Instantiate (p[Random.Range (0, p.Length)]);
+			GameObject pt=pool.GetPlaform();
+			pt.transform.position = new Vector3 (transform.position.x, transform.position.y, transform.position.z+pt.GetComponentInChildren<Collider> ().bounds.size.z);
+			transform.position = new Vector3(transform.position.x,pt.transform.position.y,transform.position.z+pt.GetComponentInChildren<Collider> ().bounds.size.z);
+		}
+
+	}
+}
---
+++ Part-3/Pooler.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pooler : MonoBehaviour {
+
+	// Use this for initialization
+	public int num;
+	public GameObject[] plats;
+	List<GameObject> pooled;
+
+
+	void Start () {
+		
+		pooled=new List<GameObject>();
+		int j=0;
+		for (int i = 0; i < num; i++) 
+		{
+			GameObject a = Instantiate (plats [j]);
+			a.SetActive (false);
+			j++;
+			if (j == plats.Length) {
+					
+				j = 0;}
+
+			pooled.Add (a);
+		}
+
+	}
+public GameObject GetPlaform()
+	{
+		for (int i = 0; i < pooled.Count; i++) {
+		
+			if (!pooled [i].activeInHierarchy) {
+			
+				pooled [i].SetActive (true);
+				return pooled [i];
+			}
+		
+		}
+		return null;
+	}
+
+
+	
+	// Update is called once per frame
+
+
+}
