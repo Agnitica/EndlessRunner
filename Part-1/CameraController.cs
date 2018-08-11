52847e923ae2b40c20018c478f4a21e74cfffc1f
Agnitica <agnitica@gmail.com>
2018-08-11 17:30:02 +0530

    Update CameraController.cs

--- Part-1/CameraController.cs
+++ Part-1/CameraController.cs
@@ -9,11 +9,11 @@ public class CameraController : MonoBehaviour {
 	private float z;
 
 	void Start () {
-		z= transform.position.z - player.transform.position.z; 
+		z= player.transform.position.z-transform.position.z; 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.position = new Vector3 (transform.position.x , transform.position.y, player.transform.position.z+z);
+		transform.position = new Vector3 (transform.position.x , transform.position.y, player.transform.position.z-z);
 	}
 }
