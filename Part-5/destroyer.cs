66d6f0e42294ee0a323567a2b21457263b8db71e
Agnitica <agnitica@gmail.com>
2018-08-19 21:16:58 +0530

    Add files via upload

---
+++ Part-5/destroyer.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroyer : MonoBehaviour {
+
+	private float dist;
+
+	public CameraController cam;
+	// Use this for initialization
+	void Start () {
+
+		dist=cam.transform.position.z-transform.position.z;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.position=new Vector3(transform.position.x,transform.position.y,cam.transform.position.z-dist);
+
+		GameObject a=GameObject.FindGameObjectWithTag ("Platform");
+		if (a != null) {
+
+
+			if (a.transform.position.z< transform.position.z) {
+
+
+				//Destroy (a);
+				a.SetActive(false);
+			
+			}
+		}
+
+		GameObject b=GameObject.FindGameObjectWithTag ("obstacles");
+		if (b != null) {
+
+
+			if (b.transform.position.z< transform.position.z) {
+
+
+				//Destroy (a);
+				b.SetActive(false);
+
+			}
+		}
+
+
+	}
+}
---
+++ Part-5/obstaclespooler.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class obstaclespooler : MonoBehaviour {
+
+	// Use this for initialization
+	public int num;
+	public GameObject[] obs;
+	List<GameObject> obspooled;
+	void Start () {
+		obspooled=new List<GameObject>();
+		int j=0;
+		for (int i = 0; i < num; i++) 
+		{
+			GameObject a = Instantiate (obs [j]);
+			a.SetActive (false);
+			j++;
+			if (j == obs.Length) {
+
+				j = 0;}
+
+			obspooled.Add (a);
+		}
+
+	}
+	
+	// Update is called once per frame
+
+
+	public GameObject GetObstacles()
+	{
+		for (int i = 0; i < obspooled.Count; i++) {
+
+			if (!obspooled [i].activeInHierarchy) {
+
+				obspooled [i].SetActive (true);
+				return obspooled [i];
+			}
+
+		}
+		GameObject o=Instantiate (obs[Random.Range(0,obs.Length)]);
+		return o;
+	}
+
+}
