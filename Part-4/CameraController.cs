e1f6b3cd111ee47af16538a5b806258851124e97
Agnitica <agnitica@gmail.com>
2018-08-21 07:44:26 +0530

    Update CameraController.cs

--- Part-4/CameraController.cs
+++ Part-4/CameraController.cs
@@ -6,25 +6,21 @@ public class CameraController : MonoBehaviour {
 
 	// Use this for initialization
 	public PlayerController player; 
-	//private Vector3 dis;
+	
 	private float z;
 	private float y;
-	//private float x;
-	//public GameObject Platform;
-//	private float y;
+	
 	void Start () {
 		z=player.transform.position.z-transform.position.z;
 		y=player.transform.position.y-transform.position.y;
-		//x=player.transform.position.x-transform.position.x;
-		//dis=player.transform.position-transform.position;
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
 		transform.position = new Vector3(transform.position.x,player.transform.position.y-y,player.transform.position.z-z);
-		//transform.position+=(player.transform.position-dis)*Time.deltaTime;
-		//transform.position = player.transform.position - dis;
+		
 	
 	}
 }
