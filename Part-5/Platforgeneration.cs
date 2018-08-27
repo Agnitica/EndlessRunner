e8179fe1918f657184cfe90840e4f0f9fad72797
Agnitica <agnitica@gmail.com>
2018-08-28 00:32:35 +0530

    Update Platforgeneration.cs

--- Part-5/Platforgeneration.cs
+++ Part-5/Platforgeneration.cs
@@ -13,9 +13,7 @@ public class Platforgeneration : MonoBehaviour {
 	public float mind;
 	public float maxd;
 	public obstaclespooler obs;
-	//public float minv;
-	//public float maxv;
-
+	
 	// Use this for initialization
 	void Start () {
 
@@ -28,13 +26,12 @@ public class Platforgeneration : MonoBehaviour {
 	void Update () {
 		if (transform.position.z - Player.transform.position.z < d.z)
 		{
-			//GameObject pt = Instantiate (p[Random.Range (0, p.Length)]);
+			
 
 			float temph = Random.Range (minh, maxh);
-		//	if (temph < 0) {
+		
 				float tempd = Random.Range (mind, maxd);
-		//	}
-		//s	float tempv = Random.Range (minv, maxv);
+		
 			GameObject pt=pool.GetPlaform();
 			pt.transform.position = new Vector3 (transform.position.x, transform.position.y+temph, transform.position.z+pt.GetComponentInChildren<Collider> ().bounds.size.z+tempd);
 			GameObject ob = obs.GetObstacles ();
@@ -46,8 +43,7 @@ public class Platforgeneration : MonoBehaviour {
 			float z = Random.Range (zmin+ob.GetComponentInChildren<Collider> ().bounds.size.z/2, zmax-ob.GetComponentInChildren<Collider> ().bounds.size.z/2);
 			float x = Random.Range (xmin+ob.GetComponentInChildren<Collider> ().bounds.size.x/2, xmax-ob.GetComponentInChildren<Collider> ().bounds.size.x/2);
 			ob.transform.position = new Vector3 (x, pt.transform.position.y, z);
-			//float x2 = pt.transform.position.z-pt.GetComponentInChildren<Collider> ().bounds.size.z;
-			//Random.Range(x1- pt.transform.position.z-pt.GetComponentInChildren<Collider> ().bounds.size.z,x1);
+			
 
 
 			transform.position = new Vector3(pt.transform.position.x,pt.transform.position.y,pt.transform.position.z);
