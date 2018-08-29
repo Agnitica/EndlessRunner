0e479cdcfefc013a4a46680d46626019cb74fe9f
Agnitica <agnitica@gmail.com>
2018-08-30 01:48:52 +0530

    Add files via upload

---
+++ Part-6/PlayerController.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+	public Rigidbody player;
+	public float movingspeed=5f;
+	public float vspeed=5.0f;
+	public float xspeed=2f;
+	private bool jump;
+	public Animator animator;
+	private Vector3 playerPosition;
+	private Quaternion playerrot;
+	public int health = 5;
+	public RectTransform gameOver;
+
+	float ktimer=0;
+	private float yposition;
+
+	// Use this for initialization
+	void Start () {
+
+
+		player =GetComponent<Rigidbody> ();
+		player.velocity = new Vector3 (0, 0, 0);
+		playerPosition= animator.transform.localPosition;
+		playerrot = animator.transform.rotation;
+		gameOver.gameObject.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (ktimer > 0) {
+		
+			player.velocity = new Vector3 (0, 0, 0);
+			animator.SetFloat ("Velocity", player.velocity.z);
+			gameOver.gameObject.SetActive (true);
+
+
+		
+
+		} else {
+			player.velocity = new Vector3 (player.velocity.x, player.velocity.y, movingspeed);
+			animator.SetFloat ("Velocity", player.velocity.z);
+		
+			if (player.transform.position.y < yposition - 40f) {
+			
+				ktimer = 1;
+			}
+
+			if ((jump) && (Input.GetKeyDown ("space"))) {    
+				player.velocity = new Vector3 (player.velocity.x, vspeed, player.velocity.z);
+				jump = false;
+			} 
+
+
+			if (Input.GetKey ("left")) {
+				//player.velocity = new Vector3 (-xspeed, 2f ,0);
+				player.transform.position += Vector3.left * xspeed * Time.deltaTime;
+				//	jump=false;
+			}//player.velocity = new Vector3 (0, 0 ,movingspeed);
+
+			if (Input.GetKey ("right")) {
+				player.transform.position += Vector3.right * xspeed * Time.deltaTime;
+				//	jump=false;
+
+			}
+			//animator.SetFloat ("Velocity", player.velocity.z);
+			animator.SetBool ("onPlatform", jump);
+		}
+
+	}
+
+
+
+	void OnCollisionEnter (Collision coll)
+	{
+		if (coll.gameObject.tag == "terrain") {			
+			jump = true;
+			yposition = player.transform.position.y;
+
+		}
+
+		if (coll.gameObject.tag == "obstacles") 
+		{
+
+			ktimer = 1f;
+		
+
+
+			}
+		
+
+
+
+	}
+
+
+	//private IEnumerator kickback(){
+
+//	Debug.Log ("hello");
+//		yield return new WaitForSeconds (1f);
+//		ktimer = 0;
+
+//	}
+
+	void LateUpdate () {
+		
+		animator.transform.localPosition =playerPosition;
+		animator.transform.localRotation = playerrot;
+	}
+
+
+}
---
+++ Part-6/restart.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class restart : MonoBehaviour {
+
+	// Use this for initialization
+	public void onRestartPress()
+	{
+		SceneManager.LoadScene ("game");
+
+	}
+	public void onMainMenu()
+	{
+
+		Application.Quit ();
+		//SceneManager.LoadScene ("menuscene");
+	
+	}
+
+
+}
